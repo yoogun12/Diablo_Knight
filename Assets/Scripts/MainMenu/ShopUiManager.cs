a8c3a57aa144cabb09dd7691b4ff925eeb3b5ef7
yoogun12 <yoogun1313@gmail.com>
2025-06-18 22:19:09 +0900

    [Feat] 상점에 현재 보유한 코인 갯수 나타내기

---
+++ Assets/Scripts/MainMenu/ShopUiManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShopUiManager : MonoBehaviour
+{
+    public TextMeshProUGUI totalCoinText;
+
+    private void OnEnable()
+    {
+        if (GameDataManager.Instance != null && GameDataManager.Instance.playerData != null)
+        {
+            int totalCoins = GameDataManager.Instance.playerData.totalCoins;
+            totalCoinText.text = "보유 코인: " + totalCoins.ToString();
+        }
+        else
+        {
+            totalCoinText.text = "보유 코인: 0";
+            Debug.LogWarning("GameDataManager 또는 playerData가 초기화되지 않았습니다.");
+        }
+    }
+}
